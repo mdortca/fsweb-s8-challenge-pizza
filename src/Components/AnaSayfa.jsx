b298b664208713f01ce55f5eef32c973a81ab084
mdortca <sedatsahin224444@gmail.com>
2025-02-22 18:08:38 +0300

    logonun anasayfanın üstüne ortalanarak konumlanması

--- src/Components/AnaSayfa.jsx
+++ src/Components/AnaSayfa.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './AnaSayfa.css';
+import logo from "../assets/logo.svg";
 
 export default function AnaSayfa() {
     return (
       <div className="ana-sayfa-container">
+        <img src={logo} alt="Logo" className="logo"/>
       </div>
     );
 }
