a45a161e846de0a2c34926ff1ad969efbaba7fad
mdortca <sedatsahin224444@gmail.com>
2025-02-24 20:10:32 +0300

    --

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import AnaSayfa from './Components/AnaSayfa'
 import './App.css'
 import SiparisOnayi from './Components/SiparisOnayi';
 import SiparisFormu from './Components/SiparisFormu';
+import { BrowserRouter as Switch, Router, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    
-<>
-<AnaSayfa />
-</>
+<div>
+    <Switch>
+      <Route path="/" >
+        <AnaSayfa />
+      </Route>
+    </Switch>
+    </div>
   )
 }
 
