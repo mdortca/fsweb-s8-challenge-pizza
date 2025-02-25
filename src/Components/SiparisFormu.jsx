ac4b3991b1c14fd5a4e2f3afbabfcb20d4e9fe0d
mdortca <sedatsahin224444@gmail.com>
2025-02-25 11:18:20 +0300

    Siparis Formu sayfasının dinamik hale gelmesinin baslangıcı

--- src/Components/SiparisFormu.jsx
+++ src/Components/SiparisFormu.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import './SiparisFormu.css';
+import logo from "../assets/logo.svg";
 
 export default function SiparisFormu() {
     return (
       <div className="siparis-formu-container">
+        <img src={logo} alt="logo" className="logo"/>
+        <button>SİPARİŞ VER </button>
       </div>
     );
 }
